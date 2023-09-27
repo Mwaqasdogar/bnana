a0ddd7f1b20ed1c9c86c38fac0861fe671a2f274
mwaqas <mwaqasdogar122@gmail.com>
2023-09-27 01:50:07 -0700

    java

---
+++ respones.js
@@ -0,0 +1,13 @@
+burger= document.querySelector('.burger')
+navlist= document.querySelector('.navlist')
+navbar= document.querySelector('.navbar')
+rightnav= document.querySelector('.rightnav')
+
+burger.addeventlistener('click', () =>{
+rightnav.classlist.toggle('v-classresp')
+navlist.classlist.toggle('v-classresp')
+h-navresp.toggle('h-navresp')
+
+
+})
+
